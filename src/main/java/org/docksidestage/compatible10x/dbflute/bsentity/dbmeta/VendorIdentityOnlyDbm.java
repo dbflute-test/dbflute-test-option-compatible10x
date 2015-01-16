c034a5cba9fdb5257f45ac5bd08e3590b51e67c1
jflute <dbflute@gmail.com>
2015-01-17 00:49:56 +0900

    patch at 2015/01/16

--- src/main/java/org/docksidestage/compatible10x/dbflute/bsentity/dbmeta/VendorIdentityOnlyDbm.java
+++ src/main/java/org/docksidestage/compatible10x/dbflute/bsentity/dbmeta/VendorIdentityOnlyDbm.java
@@ -76,7 +76,7 @@ public class VendorIdentityOnlyDbm extends AbstractDBMeta {
     // ===================================================================================
     //                                                                         Column Info
     //                                                                         ===========
-    protected final ColumnInfo _columnIdentityOnlyId = cci("IDENTITY_ONLY_ID", "IDENTITY_ONLY_ID", null, null, Long.class, "identityOnlyId", null, true, true, true, "BIGINT", 19, 0, "NEXT VALUE FOR PUBLIC.SYSTEM_SEQUENCE_480D8B23_F222_44DC_BF75_B7697C6FB5A7", false, null, null, null, null, null, false);
+    protected final ColumnInfo _columnIdentityOnlyId = cci("IDENTITY_ONLY_ID", "IDENTITY_ONLY_ID", null, null, Long.class, "identityOnlyId", null, true, true, true, "BIGINT", 19, 0, "NEXT VALUE FOR PUBLIC.SYSTEM_SEQUENCE_D2D790F7_952A_44B7_8058_7E8F40012FC8", false, null, null, null, null, null, false);
 
     /**
      * IDENTITY_ONLY_ID: {PK, ID, NotNull, BIGINT(19)}
